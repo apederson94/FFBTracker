7348e4e63b2a89338ba17ab9873156a5f5865e38
Austin Pederson <austinpederson@pm.me>
2021-10-13 19:17:21 -0700

    sorted players by name, removed Player Name header, & indented the league names for a basic UI improvement:

--- app/src/main/java/com/amped94/ffbtracker/MainActivity.kt
+++ app/src/main/java/com/amped94/ffbtracker/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
@@ -18,6 +19,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.amped94.ffbtracker.data.model.db.entity.PlayerAndLeagues
 import com.amped94.ffbtracker.data.model.db.entity.PlayerLeagueCrossRef
 import com.amped94.ffbtracker.data.model.db.entity.UserAndLeagues
@@ -59,12 +61,12 @@ fun UserInfo(playerData: List<PlayerAndLeagues>) {
 
     LazyColumn(modifier = Modifier.fillMaxWidth()) {
 
-        playerData.forEach { player ->
+        playerData.sortedBy { it.player.name }.forEach { player ->
             item {
-                CustomText(title = "Player Name: ", content = player.player.name)
+                Text(player.player.name)
 
                 player.leagues.forEach {
-                    Text(it.name)
+                    Text(it.name, modifier = Modifier.padding(start = 64.dp))
                 }
             }
         }
