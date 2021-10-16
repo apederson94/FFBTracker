42aea7c13c0032b1ce1cd1cfefc16768f9e95a51
Austin Pederson <austinpederson@pm.me>
2021-10-15 19:39:22 -0700

    cleaned up some of the LeagueSpecs code

--- app/src/main/java/com/amped94/ffbtracker/data/model/viewModel/LeagueSpecsViewModel.kt
+++ app/src/main/java/com/amped94/ffbtracker/data/model/viewModel/LeagueSpecsViewModel.kt
@@ -3,14 +3,15 @@ package com.amped94.ffbtracker.data.model.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class LeagueSpecsViewModel: ViewModel() {
+class LeagueSpecsViewModel : ViewModel() {
     val leagueName = MutableLiveData<String>()
-    val numberOfQB = MutableLiveData<Int>(1)
-    val numberOfRB = MutableLiveData<Int>(2)
-    val numberOfWR = MutableLiveData<Int>(2)
-    val numberOfTE = MutableLiveData<Int>(1)
-    val numberOfFLEX = MutableLiveData<Int>(1)
-    val numberOfK = MutableLiveData<Int>(1)
-    val numberOfDST = MutableLiveData<Int>(1)
-    val numberOfSuperFLEX = MutableLiveData<Int>(0)
+    val numberOfQB = MutableLiveData(1)
+    val numberOfRB = MutableLiveData(2)
+    val numberOfWR = MutableLiveData(2)
+    val numberOfTE = MutableLiveData(1)
+    val numberOfFLEX = MutableLiveData(1)
+    val numberOfK = MutableLiveData(1)
+    val numberOfDST = MutableLiveData(1)
+    val numberOfSuperFLEX = MutableLiveData(0)
+    val numberOfBench = MutableLiveData(7)
 }
--- app/src/main/java/com/amped94/ffbtracker/ui/composable/LeagueSpecs.kt
+++ app/src/main/java/com/amped94/ffbtracker/ui/composable/LeagueSpecs.kt
@@ -1,11 +1,7 @@
 package com.amped94.ffbtracker.ui.composable
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.DropdownMenu
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
@@ -13,7 +9,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.amped94.ffbtracker.data.model.ui.Screen
 import com.amped94.ffbtracker.data.model.viewModel.LeagueSpecsViewModel
@@ -30,52 +25,56 @@ fun LeagueSpecs(navController: NavController) {
     val numberOfK by viewModel.numberOfK.observeAsState()
     val numberOfDST by viewModel.numberOfDST.observeAsState()
     val numberOfSuperFLEX by viewModel.numberOfSuperFLEX.observeAsState()
+    val numberOfBench by viewModel.numberOfBench.observeAsState()
 
-    Column {
+    Column(modifier = Modifier.padding(8.dp)) {
         OutlinedTextField(
             value = leagueName,
             onValueChange = { leagueName = it },
             label = {
                 Text("League Name")
-            }, modifier = Modifier.padding(8.dp))
+            }, modifier = Modifier.padding(bottom = 8.dp)
+        )
 
-        Column(modifier = Modifier.padding(start = 8.dp)) {
-            PositionDropdown("$numberOfQB QB") {
-                viewModel.numberOfQB.postValue(it)
-            }
-            Spacer(Modifier.height(8.dp))
-            PositionDropdown("$numberOfRB RB") {
-                viewModel.numberOfRB.postValue(it)
-            }
-            Spacer(Modifier.height(8.dp))
-            PositionDropdown("$numberOfWR WR") {
-                viewModel.numberOfWR.postValue(it)
-            }
-            Spacer(Modifier.height(8.dp))
-            PositionDropdown("$numberOfTE TE") {
-                viewModel.numberOfTE.postValue(it)
-            }
-            Spacer(Modifier.height(8.dp))
-            PositionDropdown("$numberOfFLEX FLEX") {
-                viewModel.numberOfFLEX.postValue(it)
-            }
-            Spacer(Modifier.height(8.dp))
-            PositionDropdown("$numberOfK K") {
-                viewModel.numberOfK.postValue(it)
-            }
-            Spacer(Modifier.height(8.dp))
-            PositionDropdown("$numberOfDST D/ST") {
-                viewModel.numberOfDST.postValue(it)
-            }
-            Spacer(Modifier.height(8.dp))
-            PositionDropdown("$numberOfSuperFLEX SuperFLEX") {
-                viewModel.numberOfSuperFLEX.postValue(it)
-            }
+        PositionDropdown("$numberOfQB QB") {
+            viewModel.numberOfQB.postValue(it)
+        }
+        Spacer(Modifier.height(8.dp))
+        PositionDropdown("$numberOfRB RB") {
+            viewModel.numberOfRB.postValue(it)
+        }
+        Spacer(Modifier.height(8.dp))
+        PositionDropdown("$numberOfWR WR") {
+            viewModel.numberOfWR.postValue(it)
+        }
+        Spacer(Modifier.height(8.dp))
+        PositionDropdown("$numberOfTE TE") {
+            viewModel.numberOfTE.postValue(it)
+        }
+        Spacer(Modifier.height(8.dp))
+        PositionDropdown("$numberOfFLEX FLEX") {
+            viewModel.numberOfFLEX.postValue(it)
+        }
+        Spacer(Modifier.height(8.dp))
+        PositionDropdown("$numberOfK K") {
+            viewModel.numberOfK.postValue(it)
+        }
+        Spacer(Modifier.height(8.dp))
+        PositionDropdown("$numberOfDST D/ST") {
+            viewModel.numberOfDST.postValue(it)
+        }
+        Spacer(Modifier.height(8.dp))
+        PositionDropdown("$numberOfSuperFLEX SuperFLEX") {
+            viewModel.numberOfSuperFLEX.postValue(it)
+        }
+        Spacer(Modifier.height(8.dp))
+        PositionDropdown("$numberOfBench Bench") {
+            viewModel.numberOfBench.postValue(it)
         }
 
         Button(onClick = {
             navController.navigate(Screen.Leagues.Add.AddPlayersToLeague.route)
-        }) {
+        }, modifier = Modifier.fillMaxWidth().padding(top = 8.dp)) {
             Text("Continue")
         }
     }
